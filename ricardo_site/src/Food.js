20395f6951f5daf06b9079537ebc6d0792667df3
ricardomerlin <rcrdmerlin@gmail.com>
2024-10-16 01:31:51 -0400

    updating metadata tags, implementing additional OW features

--- ricardo_site/src/Food.js
+++ ricardo_site/src/Food.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Conversation, OpenWebProvider, PopularInTheCommunity } from '@open-web/react-sdk';
+import { Conversation, OpenWebProvider, PopularInTheCommunity, Spotlight, Reactions, TopicTracker } from '@open-web/react-sdk';
 import './styling/Food.css';
 import dishImage1 from './images/burger.webp';
 import dishImage2 from './images/pasta.jpg';
@@ -9,54 +9,62 @@ function Food() {
 
   return (
     <HelmetProvider>
-      <div className="food-container">
         <Helmet>
           <title>Food</title>
           <meta property="og:title" content="Food Page Metadata" />
           <meta property="og:url" content="https://ricardo-sso.vercel.app/food" />
           <meta property="og:description" content="Page focused on the food page" />
           <meta property="og:image" content="https://www.wholesomeyum.com/wp-content/uploads/2023/06/wholesomeyum-Best-Burger-Recipe-12.jpg" />
-          <meta property="article:tag" content="Interesting" />
-          <meta property="article:tag" content="What" />
-          <meta property="article:tag" content="Cool" />
+          <meta property="article:tag" content="Tasty" />
+          <meta property="article:tag" content="Food" />
+          <meta property="article:tag" content="Culture" />
+          <meta property="article:tag" content="Savor" />
+          <meta property="article:tag" content="Vegan" />
         </Helmet>
-        <main className="food-content">
-          <section className="food-section">
-            <h2>The Joy and Diversity of Food</h2>
-            <p>
-              Food is more than just sustenance; it is an experience that brings people together, a celebration of culture, and a form of creative expression. Every dish tells a story, whether it's a family recipe passed down through generations or a new culinary innovation pushing the boundaries of flavor and technique. 
-            </p>
-            <img src={dishImage1} alt="Delicious dish 1" className="food-image-right" />
-            <p>
-              Across the globe, food is an integral part of every society, reflecting traditions, history, and the environment. From the rich spices of Indian cuisine to the delicate flavors of Japanese sushi, the diversity of food is as vast as the world itself. Each region boasts its own unique ingredients and cooking methods, influenced by geography, climate, and cultural exchanges over centuries.
-            </p>
-            <p>
-              The way food brings people together is truly remarkable. A simple meal shared among friends can spark conversations, strengthen bonds, and create unforgettable memories. The act of cooking itself can be a form of meditation, allowing people to slow down, focus, and take joy in the process of creating something nourishing and delicious.
-            </p>
-            <p>
-              The joy of food lies in its ability to evoke memories, connect us with loved ones, and introduce us to new experiences. For many, the smell of a familiar dish can transport them back to their childhood or remind them of a special person or moment in time. Food can also be a window into different cultures, allowing us to explore unfamiliar ingredients, techniques, and flavors without ever leaving the kitchen or dining table.
-            </p>
-            <p>
-              Whether it's a home-cooked meal shared with family, a street food adventure in a bustling city, or a fine dining experience in a renowned restaurant, food has the power to create lasting impressions and bring joy to our lives. It’s not just about eating to survive; it’s about celebrating life, sharing experiences, and exploring the endless possibilities of flavors and textures that the world of food has to offer.
-            </p>
-            <img src={dishImage2} alt="Delicious dish 2" className="food-image-left" />
-            <p>
-              As our understanding of nutrition evolves, so does our approach to food. Today, there is a growing emphasis on healthy eating, sustainability, and mindful consumption. People are more conscious than ever about where their food comes from, how it is produced, and the impact it has on the planet. Farm-to-table movements, organic farming, and plant-based diets are just a few examples of how people are striving to make healthier and more ethical food choices.
-            </p>
-            <p>
-              This shift towards mindful eating doesn’t mean sacrificing taste or enjoyment. In fact, many chefs and home cooks alike are finding creative ways to elevate plant-based dishes, incorporate sustainably sourced ingredients, and reduce food waste, all while maintaining the rich, vibrant flavors that make eating such a pleasure. 
-            </p>
-            <p>
-              In the end, food is a universal language. It transcends borders and cultures, offering a way for people to connect, celebrate, and express themselves. Whether you're enjoying a simple meal at home or embarking on a culinary adventure in a foreign land, food has the power to unite us, nourish our bodies, and feed our souls.
-            </p>
-          </section>
-          <OpenWebProvider spotId='sp_5esW6NWZ'>
-            <PopularInTheCommunity postId='Food' postUrl='https://ricardo-sso.vercel.app/food' />
-            <Conversation postId='Food' className='owConv' articleTags={['tag1','tag2','tag3']} postUrl={`https://ricardo-sso.vercel.app/food`} />
-          </OpenWebProvider>
-          <div data-spotim-module="pitc"></div>
+        <OpenWebProvider spotId='sp_5esW6NWZ'>
+        <main>
+        <div className="food-container">
+          <div data-openweb-ad="" data-row="1" data-column="1" data-page-type="independent_ad_hp"></div>
+            <section className="food-section">
+              <h2>The Joy and Diversity of Food</h2>
+              <p>
+                Food is more than just sustenance; it is an experience that brings people together, a celebration of culture, and a form of creative expression. Every dish tells a story, whether it's a family recipe passed down through generations or a new culinary innovation pushing the boundaries of flavor and technique. 
+              </p>
+              <img src={dishImage1} alt="Delicious dish 1" className="food-image-right" />
+              <p>
+                Across the globe, food is an integral part of every society, reflecting traditions, history, and the environment. From the rich spices of Indian cuisine to the delicate flavors of Japanese sushi, the diversity of food is as vast as the world itself. Each region boasts its own unique ingredients and cooking methods, influenced by geography, climate, and cultural exchanges over centuries.
+              </p>
+              <p>
+                The way food brings people together is truly remarkable. A simple meal shared among friends can spark conversations, strengthen bonds, and create unforgettable memories. The act of cooking itself can be a form of meditation, allowing people to slow down, focus, and take joy in the process of creating something nourishing and delicious.
+              </p>
+              <p>
+                The joy of food lies in its ability to evoke memories, connect us with loved ones, and introduce us to new experiences. For many, the smell of a familiar dish can transport them back to their childhood or remind them of a special person or moment in time. Food can also be a window into different cultures, allowing us to explore unfamiliar ingredients, techniques, and flavors without ever leaving the kitchen or dining table.
+              </p>
+              <p>
+                Whether it's a home-cooked meal shared with family, a street food adventure in a bustling city, or a fine dining experience in a renowned restaurant, food has the power to create lasting impressions and bring joy to our lives. It’s not just about eating to survive; it’s about celebrating life, sharing experiences, and exploring the endless possibilities of flavors and textures that the world of food has to offer.
+              </p>
+              <img src={dishImage2} alt="Delicious dish 2" className="food-image-left" />
+              <p>
+                As our understanding of nutrition evolves, so does our approach to food. Today, there is a growing emphasis on healthy eating, sustainability, and mindful consumption. People are more conscious than ever about where their food comes from, how it is produced, and the impact it has on the planet. Farm-to-table movements, organic farming, and plant-based diets are just a few examples of how people are striving to make healthier and more ethical food choices.
+              </p>
+              <p>
+                This shift towards mindful eating doesn’t mean sacrificing taste or enjoyment. In fact, many chefs and home cooks alike are finding creative ways to elevate plant-based dishes, incorporate sustainably sourced ingredients, and reduce food waste, all while maintaining the rich, vibrant flavors that make eating such a pleasure. 
+              </p>
+              <p>
+                In the end, food is a universal language. It transcends borders and cultures, offering a way for people to connect, celebrate, and express themselves. Whether you're enjoying a simple meal at home or embarking on a culinary adventure in a foreign land, food has the power to unite us, nourish our bodies, and feed our souls.
+              </p>
+            </section>
+            <section className='right-section'>
+              <div data-openweb-ad="" data-row="1" data-column="1" data-page-type="independent_ad_hp"></div>
+              <TopicTracker postId='Food' postUrl='https://ricardo-sso.vercel.app/food' />
+              <Spotlight className='spotlight'/>
+              <PopularInTheCommunity postId='Food' postUrl='https://ricardo-sso.vercel.app/food' />
+            </section>
+          </div>
+          <Conversation postId='Food' className='owConv' articleTags={['tag1','tag2','tag3']} postUrl={`https://ricardo-sso.vercel.app/food`} />
+          <Reactions isSidebar='true' postId='Food' postUrl='https://ricardo-sso.vercel.app/food' />
         </main>
-      </div>
+        </OpenWebProvider>
     </HelmetProvider>
   );
 }
--- ricardo_site/src/Robots.js
+++ ricardo_site/src/Robots.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Conversation, OpenWebProvider, PopularInTheCommunity } from '@open-web/react-sdk';
+import { Conversation, OpenWebProvider, PopularInTheCommunity, Spotlight, TopicTracker, Reactions } from '@open-web/react-sdk';
 import "./styling/Robots.css";
 import teslaBot from './images/teslarobot.webp';
 import robotFace from './images/robot1.jpg';
@@ -8,46 +8,54 @@ import robotFace from './images/robot1.jpg';
 function Robots() {
   return (
     <HelmetProvider>
-      <div className="robots-container">
         <Helmet>
           <title>Robots</title>
           <meta property="og:title" content="Robots Page Metadata" />
           <meta property="og:url" content="https://ricardo-sso.vercel.app/robots" />
           <meta property="og:description" content="Page focused on the robots page" />
           <meta property="og:image" content="https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/4fc938a4-9a44-40e7-8025-3973b0c07494/width=450//CUTE_ROBOTS_FLUX_e000005_01_20240917012227.jpeg" />
-          <meta property="article:tag" content="Cool" />
-          <meta property="article:tag" content="Dumb" />
-          <meta property="article:tag" content="Amazing" />
+          <meta property="article:tag" content="Robots" />
+          <meta property="article:tag" content="Technology" />
+          <meta property="article:tag" content="Future" />
+          <meta property="article:tag" content="AI" />
+          <meta property="article:tag" content="Automation" />
         </Helmet>
-        <main className="robots-content">
-          <section className="robots-section">
-            <h2>The Growing Role of Robots in Modern Society</h2>
-            <p>
-              In the ever-evolving landscape of technology, robots have become central to numerous industries, revolutionizing the way we approach tasks that range from the mundane to the highly complex. These mechanical marvels, designed with precision and ingenuity, possess capabilities that extend far beyond the human hand. 
-            </p>
-            <img src={robotFace} className="first-robot-image" />
-            <p>
-              From autonomous drones soaring through the skies to surgical robots performing delicate operations, robots have seamlessly integrated into our daily lives, enhancing efficiency and accuracy.
-            </p>
-            <p>
-              As advancements in artificial intelligence continue to accelerate, robots are becoming increasingly sophisticated. They can learn from their environment, adapt to new situations, and even exhibit forms of artificial empathy, making them invaluable in sectors like healthcare, manufacturing, and exploration.
-            </p>
-            <p>
-              The potential of robots is virtually limitless, promising a future where they will not only assist but also innovate alongside us, driving humanity towards new frontiers.
-            </p>
-            <p>
-              While the fear of robots replacing human jobs persists, many argue that these machines are creating new opportunities, enabling people to focus on more creative and strategic endeavors. 
-            </p>
-            <div className="second-image-container">
-              <img src={teslaBot} className="second-robot-image" />
-            </div>
-          </section>
-          <OpenWebProvider spotId='sp_5esW6NWZ'>
-            <PopularInTheCommunity postId='Robots' postUrl='https://ricardo-sso.vercel.app/robots' />
-            <Conversation postId='Robots' className='owConv' articleTags={['tag1','tag2','tag3']} postUrl={`https://ricardo-sso.vercel.app/robots`} />
-          </OpenWebProvider>
+        <OpenWebProvider spotId='sp_5esW6NWZ'>
+        <main>
+          <div className="robots-container">
+            <section className="robots-section">
+              <h2>The Growing Role of Robots in Modern Society</h2>
+              <p>
+                In the ever-evolving landscape of technology, robots have become central to numerous industries, revolutionizing the way we approach tasks that range from the mundane to the highly complex. These mechanical marvels, designed with precision and ingenuity, possess capabilities that extend far beyond the human hand. 
+              </p>
+              <img src={robotFace} className="first-robot-image" />
+              <p>
+                From autonomous drones soaring through the skies to surgical robots performing delicate operations, robots have seamlessly integrated into our daily lives, enhancing efficiency and accuracy.
+              </p>
+              <p>
+                As advancements in artificial intelligence continue to accelerate, robots are becoming increasingly sophisticated. They can learn from their environment, adapt to new situations, and even exhibit forms of artificial empathy, making them invaluable in sectors like healthcare, manufacturing, and exploration.
+              </p>
+              <p>
+                The potential of robots is virtually limitless, promising a future where they will not only assist but also innovate alongside us, driving humanity towards new frontiers.
+              </p>
+              <p>
+                While the fear of robots replacing human jobs persists, many argue that these machines are creating new opportunities, enabling people to focus on more creative and strategic endeavors. 
+              </p>
+              <div className="second-image-container">
+                <img src={teslaBot} className="second-robot-image" />
+              </div>
+            </section>
+            <section className="right-section">
+              <div data-openweb-ad="" data-row="1" data-column="1" data-page-type="independent_ad_hp"></div>
+              <TopicTracker postId='Robots' postUrl='https://ricardo-sso.vercel.app/robots' />
+              <Spotlight className='spotlight'/>
+              <PopularInTheCommunity postId='Robots' postUrl='https://ricardo-sso.vercel.app/robots' />
+            </section>
+          </div>
+          <Conversation postId='Robots' className='owConv' articleTags={['tag1','tag2','tag3']} postUrl={`https://ricardo-sso.vercel.app/robots`} />
+          <Reactions isSidebar='true' postId='Robots' postUrl='https://ricardo-sso.vercel.app/robots' />
         </main>
-      </div>
+        </OpenWebProvider>
     </HelmetProvider>
   );
 }
